82a5d19c0255fe20c332b42b7c10c6def4a70e79
Yana.Novikova <Zyf.15@mail.ru>
2018-06-18 11:11:22 +0300

    Вспомогательные классы и методы vre 1.0

--- test_add_group.py
+++ test_add_group.py
@@ -63,7 +63,7 @@ class test_add_group(unittest.TestCase):
         self.return_to_groups_page(wd)
         self.logout(wd)
 
-    def est_add_empty_group(self):
+    def test_add_empty_group(self):
         wd = self.wd
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
