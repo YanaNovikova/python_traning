cccb505d649249468dcde6c5f0191b303316c34d
Yana.Novikova <Zyf.15@mail.ru>
2018-06-28 20:54:00 +0300

    Удалено дублирование логинов и логаутов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,12 +3,9 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create1(Contact(firstname="test", middlename="test", lastname="test", nickname="test", title="test", company="test", address="test", home="test", mobile="test", work="test", fax="test", email="test", email2="test", email3="test", homepage="test", byear="1995", address2="test", phone2="test", notes="test"))
-    app.session.logout()
 
 
 def test_add_empty_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.create2(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="", address="", home="", mobile="", work="", fax="", email="", email2="", email3="", homepage="", byear="", address2="", phone2="", notes=""))
-    app.session.logout()
+
--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -3,6 +3,5 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first_contact(Contact(firstname="1", middlename="1", lastname="1", nickname="1", title="1", company="1", address="1", home="1", mobile="1", work="1", fax="1", email="1", email2="1", email3="1", homepage="1", byear="2000", address2="1", phone2="1", notes="1"))
-    app.session.logout()
+
