8810df5e326e537eb3bd50372c28b9b148796f14
Yana.Novikova <Zyf.15@mail.ru>
2018-07-05 14:09:58 +0300

    Получение списка групп

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -2,4 +2,9 @@ from model.group import Group
 
 
 def test_edit_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test", header="test", footer="testtest"))
+    old_groups = app.group.get_group_list()
     app.group.edit_first_group(Group(name="1", header="2", footer="3"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
