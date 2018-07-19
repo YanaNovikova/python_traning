0058afbadbbc149b1ac39b69a605a1d342c39830
Yana.Novikova <Zyf.15@mail.ru>
2018-07-19 14:42:49 +0300

    DZ19 ver1

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -4,8 +4,8 @@ import pytest
 from data.contacts import testdata
 
 
-@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
-def test_add_contact(app, contact):
+def test_add_contact(app, json_contacts):
+    contact = json_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.create1(contact)
     assert len(old_contacts) + 1 == app.contact.count()
