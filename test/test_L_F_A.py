e9f7d08a9f339d43ff322020c35d74243b899a78
Yana.Novikova <Zyf.15@mail.ru>
2018-07-19 10:18:30 +0300

    улучшение2

--- test/test_L_F_A.py
+++ test/test_L_F_A.py
@@ -1,3 +1,4 @@
+import re
 from random import randrange
 
 
@@ -6,7 +7,9 @@ def test_phones_on_viewpage(app):
     index = randrange(len(old_contacts))
     contact_from_homepage = app.contact.get_contact_list()[index]
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_homepage.lastname == contact_from_edit_page.lastname
-    assert contact_from_homepage.firstname == contact_from_edit_page.firstname
-    assert contact_from_homepage.address == contact_from_edit_page.address
+    assert contact_from_homepage.lastname == clear(contact_from_edit_page.lastname)
+    assert contact_from_homepage.firstname == clear(contact_from_edit_page.firstname)
+    assert contact_from_homepage.address == clear(contact_from_edit_page.address)
 
+def clear(s):
+    return re.sub("[() -]", "", s)
