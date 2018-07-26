f91740120d0cf05a75c9e5efb65422e8c1255b47
Yana.Novikova <Zyf.15@mail.ru>
2018-07-26 15:57:21 +0300

    DZ20 ver2

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -3,7 +3,7 @@ from model.contact import Contact
 import random
 
 
-def test_del_first_contact(app, db):
+def test_del_first_contact(app, db, check_ui):
     if len(db.get_contact_list()) == 0:
         app.contact.create1(Contact(firstname="1", middlename="1", lastname="1", nickname="1", title="1", company="1", address="1",home="1", mobile="1", work="1", fax="1", email="1", email2="1", email3="1", homepage="1",byear="2000", address2="1", phone2="1", notes="1"))
     old_contacts = db.get_contact_list()
@@ -13,4 +13,6 @@ def test_del_first_contact(app, db):
     assert len(old_contacts) - 1 == len(new_contacts)
     old_contacts.remove(contact)
     assert old_contacts == new_contacts
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_max) == sorted(app.contact.get_contact_list(), key=Contact.id_max)
 
