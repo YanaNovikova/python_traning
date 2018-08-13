61334d6e45d5fc4ccf8ef3d526f0da319619c37a
Yana Novikova <Zyf.15@mail.ru>
2018-08-13 15:05:31 +0300

    DZ22 ver 12

--- test/test_contact_to_group.py
+++ test/test_contact_to_group.py
@@ -20,7 +20,7 @@ def test_add_contact_to_group(app, db):
         app.contact.create1(Contact(firstname="1", middlename="1", lastname="1", nickname="1", title="1", company="1", address="1",home="1", mobile="1", work="1", fax="1", email="1", email2="1", email3="1", homepage="1",byear="2000", address2="1", phone2="1", notes="1"))
     app.contact.add_contact_to_group(index, group.id)
     new_contacts = db.get_contact_list()
-    assert len(contacts) == len(new_contacts)
+    contacts.append(index)
     assert sorted(contacts, key=Contact.id_max) == sorted(new_contacts, key=Contact.id_max)
 
 
@@ -38,5 +38,6 @@ def test_delete_contact_to_group(app, db):
         app.contact.add_contact_to_group(index, group.id)
     app.contact.delete_contact_to_group(index, group.id)
     new_contacts = db.get_contact_list()
-    assert len(contacts) == len(new_contacts)
-    assert sorted(contacts, key=Contact.id_max) == sorted(new_contacts, key=Contact.id_max)
+    assert len(contacts)-1 == len(new_contacts)
+    contacts.remove(index)
+    assert contacts == new_contacts
